cc1c20019b90b490f8b9329e3e84aa5d00b3bc6e
robpap96 <robertopapagni1996@outlook.it>
2022-10-26 11:50:17 +0200

    ok

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -1,10 +1,10 @@
 // Il software deve chiedere per 10 volte all’utente di inserire un numero.
 // Il programma stampa la somma di tutti i numeri inseriti.
 "use strict";
-const tot = 0;
+let tot = 0;
 
 for(let i = 0; i < 10; i++) {
-    numeroDaSommare =  prompt(`insrisci il ${i} numero da sommare`);
-    const tot += numeroDaSommare;
+    const number = Number(prompt(`insrisci un numero`)) ;
+     tot += number;
 }
-console.log(`${tot}`);
+console.log(`totale = ${tot}`);
