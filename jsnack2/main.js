8a00152b8bfcce5a9dc1aae36837106dfa2b260b
robpap96 <robertopapagni1996@outlook.it>
2022-10-26 11:30:02 +0200

    aggiustato

--- jsnack2/main.js
+++ jsnack2/main.js
@@ -6,9 +6,9 @@ const parola1 = prompt("inserisci la prima parola");
 const parola2 = prompt("inserisci la seconda parola");
 
 if(parola1.length < parola2.length){
-    console.log(`${parola1}`);
-    console.log(`${parola2}`);
+    console.log(`${parola1} ${parola2}`);
+} else if(parola1.length > parola2.length) {
+    console.log(`${parola2} ${parola1}`);
 } else {
-    console.log(`${parola2}`);
-    console.log(`${parola1}`);
+    console.log("le parole sono della stessa lunghezza");
 }
